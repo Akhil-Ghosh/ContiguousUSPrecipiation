1c8d35ae3c6e7ae101dd24911f87a26b8e848b37
breakwavegames <125612599+breakwavegames@users.noreply.github.com>
2023-12-03 14:24:33 -0600

    changed it to download the .rds file from dropbox.

--- global.R
+++ global.R
@@ -9,6 +9,7 @@ library(rnaturalearth)
 library(rnaturalearthdata)
 library(animation)
 library(RCurl)
+library(httr)
 
 source("plotting_extreme.R")
 source("plot_day.R")
@@ -22,7 +23,21 @@ cpcRes      <- 0.25 # data resolution
 cpcLatVec   <- 20.125 + (1:cpcNumLat)*cpcRes - cpcRes # latitudes
 cpcLonVec   <- -129.875 + (1:cpcNumLon)*cpcRes - cpcRes # longitudes
 
-#upload dataset to google drive
+# code to download file from dropbox.
+# Update later for more secure/perm solution
+url <- "https://www.dropbox.com/scl/fi/s4mdrh19o54sbwnootpu2/1948_2022_precipitation_data.rds?rlkey=xoyvqrb432mupy6xpn4khgba1&dl=1" # Replace with your actual Dropbox file link
+
+# Specify a local path to save the file
+output_file <- "1948_2022_precipitation_data.rds"
+
+# Check if the file already exists
+if (!file.exists(output_file)) {
+  download.file(url, destfile = output_file, mode = "wb")
+  message("File downloaded from Dropbox.")
+} else {
+  message("File already exists locally. No download needed.")
+}
+
 precipitation_data<- readRDS('1948_2022_precipitation_data.rds')
 
 
