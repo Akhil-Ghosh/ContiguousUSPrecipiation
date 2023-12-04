9037e6ec809a5d1aa31ac532b583b67fb0b8a5bd
Akhil-Ghosh <91097186+Akhil-Ghosh@users.noreply.github.com>
2023-12-04 14:04:08 -0600

    fixing year variable and updating ui

--- ui.R
+++ ui.R
@@ -1,31 +1,33 @@
 fluidPage(
   titlePanel("Precipitation Data Visualization"),
-  
   sidebarLayout(
     sidebarPanel(
       wellPanel(
         title = h4("Annual Precipitation Range"),
-        dateInput("start_date", "Start Date", value = "2021-01-01", startview = "year"),
-        dateInput("end_date", "End Date", value = "2022-01-02", startview = "year"),
+        fluidRow(
+          column(6, 
+                 dateInput("start_date", "Start Date", value = "2022-01-01", startview = "year")
+          ),
+          column(6, 
+                 dateInput("end_date", "End Date", value = "2022-12-31", startview = "year")
+          )
+        ),
         numericInput("min_precip", "Minimum Precipitation (mm)", value = 0),
-        numericInput("max_precip", "Maximum Precipitation (mm)", value = 1000),
+        numericInput("max_precip", "Maximum Precipitation (mm)", value = 2000),
         actionButton("toggle_plot", "Toggle Plot"),
-        downloadButton("save_plot", "Save Plot"),
+        #downloadButton("save_plot", "Save Plot"),
         textOutput("current_plot_label")
       ),
-      
       wellPanel(
         title = h4("Extreme Events"),
-        numericInput("year_extreme", "Year", value = 1983, min = 1950, max = 2022),
+        numericInput("year_extreme", "Year", value = 2022, min = 1950, max = 2022),
         sliderInput("percentile", "Percentile", min = 0.9, max = 1, value = 0.99, step = 0.01)
       ),
-      
       wellPanel(
         title = h4("Day Plot"),
-        dateInput("day_date", "Select Date", value = "1983-01-01", startview = "year")
+        dateInput("day_date", "Select Date", value = "2022-01-01", startview = "year")
       )
     ),
-    
     mainPanel(
       tabsetPanel(
         tabPanel("Annual Precipitation Range", plotOutput("annual_plot")),
